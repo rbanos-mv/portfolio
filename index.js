91eda422a1bfaee07937e1cd2577dd4b9805b5aa
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-11-04 05:30:16 -0600

    Update dates

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ class Projects {
       // },
       {
         title: 'Rent-A-Green',
-        date: 'October, 2022',
+        date: 'September, 2022',
         description:[
           "<strong>Rent-A-Green</strong> is an application that allow users to book appointments for renting an electric car. The homepage is a showcase of the available cars. Users have the following options: signup, login, view the details of a selected car and book an appointment. Logged users can also add new car, or delete an existing one. The front-end was developed with ReactJS and Redux. The back-end API was developed with Ruby on Rails.",
           "I teamed with <a href=\"https://github.com/acolombo1\" target=\"_blank\">Aldo Aníbal Colombo</a> and <a href=\"https://github.com/alexpuente0\" target=\"_blank\">Alejandro Puente</a> to develop this application. We used a Kanban board to track the progress of the project. Each of us took a similar workload. This was our final capstone project to receive the <a href=\"https://www.credential.net/fd9ad20c-809d-4fe9-977b-d150c8c6d5d9\" target=\"_blank\">Microverse's Full-Stack Web Developer Certificate</a>.",
@@ -85,7 +85,7 @@ class Projects {
       },
       {
         title: 'XpenseS',
-        date: 'October, 2022',
+        date: 'September, 2022',
         description:[
           "<strong>XpenseS</strong> is a mobile web application where you can manage your budget. The users can create categories to group their expenses. This way they can see how much money they spent in each of the categories. The front-end and back-end where developed as a single Ruby on Rails application.",
           "I developed this application as the capstone project to receive the <a href=\"https://www.credential.net/a529cdb0-a5c1-49ca-a948-7c4604dd488f\" target=\"_blank\">Microverse's Ruby-on-Rails Certificate</a>."
@@ -97,7 +97,7 @@ class Projects {
       },
       {
         title: 'Recipe App',
-        date: 'Sep, 2022',
+        date: 'August, 2022',
         description:[
           "The <strong>Recipe app</strong> keeps track of all your recipes, ingredients, and inventory. It will allow you to save ingredients, keep track of what you have, create recipes, and generate a shopping list based on what you have and what you are missing from a recipe. Also, since sharing recipes is an important part of cooking the app should allow you to make them public so anyone can access them.",
           "I teamed with <a href=\"https://github.com/rega1237\" target=\"_blank\">Rafael E. Guzman Arias</a> to developed this application as a requirement to receive the <a href=\"https://www.credential.net/a529cdb0-a5c1-49ca-a948-7c4604dd488f\" target=\"_blank\">Microverse's Ruby-on-Rails Certificate</a>."
@@ -109,19 +109,19 @@ class Projects {
       },
       {
         title: 'Latin America Population',
-        date: 'xxxx, 2022',
+        date: 'June, 2022',
         description:[
           "<strong>Latin America Population</strong> is a single page application created with React and Redux that allow users to review the population of Latin America countries. This webapp uses the World Bank indicators API.",
           "I developed this application as the capstone project to receive the <a href=\"https://www.credential.net/deedcc82-1863-421e-a302-e97f49f5662d\" target=\"_blank\">Microverse's React/Redux Certificate</a>."
         ],
         images: ['images/Population.png'],
-        technologies: ['JavaScript-ES6', 'CSS3', 'ReactJS', 'Redux', 'TESTING?'],
+        technologies: ['JavaScript-ES6', 'CSS3', 'ReactJS', 'Redux', 'React Testing Library'],
         liveVersion: 'https://rbanos-mv.github.io/capstone-3-metrics/',
         source: 'https://github.com/rbanos-mv/capstone-3-metrics',
       },
       {
         title: 'Meal Time',
-        date: 'xxxx, 2022',
+        date: 'May, 2022',
         description:[
           'The <strong>Meal Time</strong> website is an HTML/Javascript project that uses two external APIs. We selected the <a href="https://www.themealdb.com/api.php">Free Meal API</a> by <a href="https://www.themealdb.com/">TheMealDB</a> which provides data for meals to build the web app around it. We also used the <a href="https://www.notion.so/microverse/Involvement-API-869e60b5ad104603aa6db59e08150270">Involvement API</a> by <a href="https://www.microverse.org/">Microverse</a> to record <em>likes</em> and <em>comments</em>.',
           "I developed this application together with <a href=\"https://github.com/aimalamiri\" target=\"_blank\">Aimal Amiri</a> as the capstone project to receive the <a href=\"https://www.credential.net/18e91d6a-98c7-4440-84e0-b6cd5c921765\" target=\"_blank\">Microverse's Javascript Certificate</a>."
